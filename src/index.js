d647631681dd0fcf1a25ab71e50eb8ea61b0f36a
loloSun <17319051427@163.com>
2019-05-27 21:36:46 +0800

    首页样式

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import '../public/img'
+import Home from './page/Home';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import $ from 'jquery'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Home />, document.getElementById('root'));
+
 serviceWorker.unregister();
---
+++ src/page/Home.js
@@ -0,0 +1,80 @@
+import React,{Component} from 'react'
+import '../static/css/Home.css'
+import $ from 'jquery'
+
+class Home extends Component{
+    constructor(props){   // 构造方法 对对象的初始化 只要存在constructor就要调用super()，但是，不是每个react组件都需要constructor，当没有调用super()时，this还没有被初始化，所以不能使用
+        super(props)   // 继承  当需要在constructor中访问this.props的情况下，必须要调用super(props)
+    }
+
+    // 挂载
+    componentDidMount(){
+        $(".list li").each(function(index){
+            // $(this).animate({
+            //     marginLeft:'0px'
+            // },500)
+            console.log(index)
+            let time = index*100
+            console.log(time)
+            $('.list').eq(index).animate({
+                marginLeft:'0px'
+            },500+(index*100))
+            // console.log($(this))
+
+        })
+        // $('.logo_img img').animate({
+        //     width:"300px"
+        // },500)
+    }
+
+    render() {
+        const pageImg = {
+            background:'url(' + require("../static/img/cover_bg_3.jpg") + ')'
+        }
+        return (
+            <div className="page" style={pageImg}>
+                {/* 遮罩 */}
+                <div className="cover"></div>
+                {/* 内容 */}
+                <div className="container">
+                    <div className="logo_img">
+                        <img src={require("../static/img/logo_img.jpg")} alt="" />
+                    </div>
+                    <h1>
+                        <span>孙晓琪</span>
+                    </h1>
+                    <h3>
+                        <span>web 前端开发工程师</span>
+                    </h3>
+                </div>
+                <ul className="list">
+                    {/* <div class="set">
+                        <img src="../img/set.png" alt="">
+                    </div> */}
+                    <li>
+                        <a href="aboutMe.html">
+                            <span>About Me</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="javascript: void(0);">
+                            <span>技能</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../html/project.html">
+                            <span>项目经验</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../html/article.html">
+                            <span>文章动态</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Home
