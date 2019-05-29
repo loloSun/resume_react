1950b25fff62d4a4b36e671d082def755ea2d116
loloSun <17319051427@163.com>
2019-05-29 10:02:50 +0800

    about me

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 ## 基本配置
-constructor(props){   
-    // 构造方法 对对象的初始化 只要存在constructor就要调用super()，但是，不是每个react组件都需要constructor，当没有调用super()时，this还没有被初始化，所以不能使用
-        super()   
-        // 继承  当需要在constructor中访问this.props的情况下，必须要调用super(props)
-    }
-##
+```
+constructor(props){   // 构造方法 对对象的初始化 只要存在constructor就要调用super()，但是，不是每个react组件都需要constructor，当没有调用super()时，this还没有被初始化，所以不能使用
+    super()   // 继承  当需要在constructor中访问this.props的情况下，必须要调用super(props)
+}
+```
+
 ## 实时获取屏幕宽度
 ```
 $(window).resize(function(){
     ···
 })
+```
+
+## 媒体查询 常见尺寸
+```
+@media screen and (min-width:1200px)
+@media screen and (min-width:992px)
+@media screen and (min-width:768px)
+@media screen and (min-width:480px)
 ```
 
 ## react 路由
--- src/page/paging/AboutMe.js
+++ src/page/paging/AboutMe.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react'
 
+import '../../static/css/aboutme.css'
+
 import Nav from '../../component/Nav'
 
 class AboutMe extends Component{
@@ -8,10 +10,57 @@ class AboutMe extends Component{
     }
 
     render(){
+        const QQimg = {
+            backgroundImage :'url(' + require("../../static/img/icon/QQ.png") + ')'
+        }
+        const wximg = {
+            backgroundImage:'url(' + require("../../static/img/icon/wx.png") + ')'
+        }
         return(
             <div className='page'>
                 <Nav />
-                AboutMe
+                <div className="container">
+                    <h2 className="title">About Me</h2>
+                    <div className="cont">
+                        <ul className="cont1">
+                            <li>
+                                <span className="first-block">Full Name:</span>
+                                <span className="second-block">孙晓琪</span>
+                            </li>
+                            <li>
+                                <span className="first-block">Phone:</span>
+                                <span className="second-block">12345678910</span>
+                            </li>
+                            <li>
+                                <span className="first-block">Email:</span>
+                                <span className="second-block">17319051427@160.com</span>
+                            </li>
+                            <li>
+                                <span className="first-block">github:</span>
+                                <span className="second-block">https://github.com/loloSun</span>
+                            </li>
+                            <li>
+                                <span className="first-block">Address:</span>
+                                <span className="second-block">北京 昌平</span>
+                            </li>
+                            <ol>
+                                <li>
+                                    <i style={QQimg}></i>
+                                    <img src={require("../../static/img/icon/qq.jpg")}></img>
+                                </li>
+                                <li>
+                                    <i style={wximg}></i>
+                                    <img src={require("../../static/img/icon/weixin.png")}></img>
+                                </li>
+                            </ol>
+                        </ul>
+                        <div className="cont2">
+                            <h2>welcome!</h2>
+                            <p>There live the blind texts far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in bookmarksgrove there live the blind texts far from the countries.</p>
+                            <p>Far far away, behind the word mountains, Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci there live the blind texts from the countries Vokalia and Consonantia, there live the blind texts. Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
