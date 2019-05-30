702a8d34eb62f907875c4aa96e4315c742d62772
loloSun <17319051427@163.com>
2019-05-30 13:55:18 +0800

    skills

--- src/page/paging/Skill.js
+++ src/page/paging/Skill.js
@@ -2,151 +2,211 @@ import React,{Component} from 'react'
 
 import Nav from '../../component/Nav'
 import '../../static/css/skill.css'
+import $ from 'jquery'
 
 class Skill extends Component{
     constructor(props){
-        super()
+        super(props)
     }
 
     // 挂载
     componentDidMount(){
-            // $('.bigone').animate({
-            //     marginLeft:'0px'
-            // },500+(index*200))
+        let flag = false
+        $(window).on('scroll',()=>{
+            if($(window).scrollTop() > 100&&!flag){
+                flag = true
+                $(".progress_bar").each(function(index){
+                    console.log($(this).data("width"))
+                    $(this).animate({
+                        width:$(this).data("width")
+                    },500)
+                })
+            }
+        })
     }
 
     render(){
         return(
-            <div className='page' style={{
-                background: '#fff!important',
-                color:'#FF9000!important'
-            }}>
+            <div className='page skill'>
                 <Nav />
-                <h2 className='title'>Skills</h2>
-                <div className='circular'>
-                    <ul>
-                        <li>
-                            <div className='bigone'>
-                                <div className="middle_1" style={{transform: "rotate(-54deg)",transformOrigin:"right"}}>
-                                    <div className='inone'></div>
+                <div className="content">
+                    <h2 className='title'>Skills</h2>
+                    <div className='circular'>
+                        <ul>
+                            <li>
+                                <div className='bigone'>
+                                    <div className="middle_1" style={{transform: "rotate(-54deg)",transformOrigin:"right"}}>
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className="middle_2">
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className='smallone'>
+                                        <span>
+                                            <strong>HTML5</strong>
+                                            <br/>
+                                            85%
+                                        </span>
+                                    </div>
                                 </div>
-                                <div className="middle_2">
-                                    <div className='inone'></div>
+                            </li>
+                            <li>
+                                <div className='bigone'>
+                                    <div className="middle_1" style={{transform: "rotate(-72deg)",transformOrigin:"right"}}>
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className="middle_2">
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className='smallone'>
+                                        <span>
+                                            <strong>CSS 3</strong>
+                                            <br/>
+                                            80%
+                                        </span>
+                                    </div>
                                 </div>
-                                <div className='smallone'>
-                                    <span>
-                                        <strong>HTML5</strong>
-                                        <br/>
-                                        85%
-                                    </span>
+                            </li>
+                            <li>
+                                <div className='bigone'>
+                                    <div className="middle_1" style={{transform: "rotate(-72deg)",transformOrigin:"right"}}>
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className="middle_2">
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className='smallone'>
+                                        <span>
+                                            <strong>JavaScript</strong>
+                                            <br/>
+                                            80%
+                                        </span>
+                                    </div>
                                 </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='bigone'>
-                                <div className="middle_1" style={{transform: "rotate(-72deg)",transformOrigin:"right"}}>
-                                    <div className='inone'></div>
+                            </li>
+                            <li>
+                                <div className='bigone'>
+                                    <div className="middle_1" style={{transform: "rotate(-108deg)",transformOrigin:"right"}}>
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className="middle_2">
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className='smallone'>
+                                        <span>
+                                            <strong>jquery</strong>
+                                            <br/>
+                                            70%
+                                        </span>
+                                    </div>
                                 </div>
-                                <div className="middle_2">
-                                    <div className='inone'></div>
+                            </li>
+                            <li>
+                                <div className='bigone'>
+                                    <div className="middle_1" style={{transform: "rotate(-90deg)",transformOrigin:"right"}}>
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className="middle_2">
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className='smallone'>
+                                        <span>
+                                            <strong>VUE</strong>
+                                            <br/>
+                                            75%
+                                        </span>
+                                    </div>
                                 </div>
-                                <div className='smallone'>
-                                    <span>
-                                        <strong>CSS 3</strong>
-                                        <br/>
-                                        80%
-                                    </span>
+                            </li>
+                            <li>
+                                <div className='bigone'>
+                                    <div className="middle_1" style={{transform: "rotate(-180deg)",transformOrigin:"right"}}>
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className="middle_2">
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className='smallone'>
+                                        <span>
+                                            <strong>react</strong>
+                                            <br/>
+                                            50%
+                                        </span>
+                                    </div>
                                 </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='bigone'>
-                                <div className="middle_1" style={{transform: "rotate(-72deg)",transformOrigin:"right"}}>
-                                    <div className='inone'></div>
+                            </li>
+                            <li>
+                                <div className='bigone'>
+                                    <div className="middle_1" style={{transform: "rotate(-90deg)",transformOrigin:"right"}}>
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className="middle_2">
+                                        <div className='inone'></div>
+                                    </div>
+                                    <div className='smallone'>
+                                        <span>
+                                            <strong>微信小程序</strong>
+                                            <br/>
+                                            75%
+                                        </span>
+                                    </div>
                                 </div>
-                                <div className="middle_2">
-                                    <div className='inone'></div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='progresses'>
+                        <ul>
+                            <li>
+                                <span>HTML5</span>
+                                <span style={{float:"right"}}>85%</span>
+                                <div className='progress'>
+                                    <div className="progress_bar" data-width='85%'></div>
                                 </div>
-                                <div className='smallone'>
-                                    <span>
-                                        <strong>JavaScript</strong>
-                                        <br/>
-                                        80%
-                                    </span>
+                            </li>
+                            <li>
+                                <span>CSS3</span>
+                                <span style={{float:"right"}}>80%</span>
+                                <div className='progress'>
+                                    <div className="progress_bar" data-width='80%'></div>
                                 </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='bigone'>
-                                <div className="middle_1" style={{transform: "rotate(-108deg)",transformOrigin:"right"}}>
-                                    <div className='inone'></div>
+                            </li>
+                            <li>
+                                <span>JavaScript</span>
+                                <span style={{float:"right"}}>80%</span>
+                                <div className='progress'>
+                                    <div className="progress_bar" data-width='80%'></div>
                                 </div>
-                                <div className="middle_2">
-                                    <div className='inone'></div>
+                            </li>
+                            <li>
+                            <span>jquery</span>
+                                <span style={{float:"right"}}>70%</span>
+                                <div className='progress'>
+                                    <div className="progress_bar" data-width='70%'></div>
                                 </div>
-                                <div className='smallone'>
-                                    <span>
-                                        <strong>jquery</strong>
-                                        <br/>
-                                        70%
-                                    </span>
+                            </li>
+                            <li>
+                                <span>VUE</span>
+                                <span style={{float:"right"}}>75%</span>
+                                <div className='progress'>
+                                    <div className="progress_bar" data-width='75%'></div>
                                 </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='bigone'>
-                                <div className="middle_1" style={{transform: "rotate(-90deg)",transformOrigin:"right"}}>
-                                    <div className='inone'></div>
+                            </li>
+                            <li>
+                                <span>React</span>
+                                <span style={{float:"right"}}>50%</span>
+                                <div className='progress'>
+                                    <div className="progress_bar" data-width='50%'></div>
                                 </div>
-                                <div className="middle_2">
-                                    <div className='inone'></div>
+                            </li>
+                            <li>
+                                <span>微信小程序</span>
+                                <span style={{float:"right"}}>75%</span>
+                                <div className='progress'>
+                                    <div className="progress_bar" data-width='75%'></div>
                                 </div>
-                                <div className='smallone'>
-                                    <span>
-                                        <strong>VUE</strong>
-                                        <br/>
-                                        75%
-                                    </span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='bigone'>
-                                <div className="middle_1" style={{transform: "rotate(-180deg)",transformOrigin:"right"}}>
-                                    <div className='inone'></div>
-                                </div>
-                                <div className="middle_2">
-                                    <div className='inone'></div>
-                                </div>
-                                <div className='smallone'>
-                                    <span>
-                                        <strong>react</strong>
-                                        <br/>
-                                        50%
-                                    </span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='bigone'>
-                                <div className="middle_1" style={{transform: "rotate(-90deg)",transformOrigin:"right"}}>
-                                    <div className='inone'></div>
-                                </div>
-                                <div className="middle_2">
-                                    <div className='inone'></div>
-                                </div>
-                                <div className='smallone'>
-                                    <span>
-                                        <strong>微信小程序</strong>
-                                        <br/>
-                                        75%
-                                    </span>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div className='progress'>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
